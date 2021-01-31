ebe1b59d274f75581b23a69a464dfcc49258cb8d
GHOST <ghostdevbusiness@gmail.com>
2021-01-31 18:46:57 +0000

    fix: app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import App from './App.svelte';
-import svelte from 'svelte/compiler';
 
 const app = new App({
-	target: document.body,
-	props: {
-		version: svelte.VERSION
-	}
+    target: document.body,
+    props: {},
 });
 
 export default app;

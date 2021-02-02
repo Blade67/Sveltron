8907170ae3ed21b50ffb9f5b10e3a0f5e5137d1c
GHOST <ghostdevbusiness@gmail.com>
2021-02-02 10:21:18 +0000

    chore: update rollup commonjs plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
@@ -24,7 +24,7 @@ export default {
         // npm, you'll most likely need these plugins. In
         // some cases you'll need additional configuration —
         // consult the documentation for details:
-        // https://github.com/rollup/rollup-plugin-commonjs
+        // https://www.npmjs.com/package/rollup-plugin-commonjs
         resolve(),
         commonjs(),
 
